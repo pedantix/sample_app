e58456ddf1f6d3fc74146081b90c0e8b406a53f7
Shaun Hubbard <shaunhubbard@Bills-MacBook-Pro.local>
2013-05-15 04:22:34 -0400

    napping, on section 9.3

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Authentication" do
 	describe "signin page" do
 		before { visit signin_path }
 
-		it {should have_selector('h1',		text: 'Sign in') }	
+		it {should have_selector('h1',	text: 'Sign in') }	
 		it {should have_title_content('Sign in') } 
 
 		describe "with invalid information" do
@@ -25,10 +25,11 @@ describe "Authentication" do
 		describe "with valid information" do
 			let(:user) { FactoryGirl.create(:user) }
 
-			before { valid_signin( user ) }
+			before { sign_in( user ) }
 
 			it { should have_title_content( user.name ) }
 			it { should have_link('Profile', href: user_path(user)) }
+			it { should have_link('Settings', href: edit_user_path(user) ) }
 			it { should have_link('Sign out', href: signout_path ) }
 			it { should_not have_link('Sign in', href: signin_path)}
 
@@ -37,5 +38,57 @@ describe "Authentication" do
 				it {should have_link('Sign in') }
 			end
 		end 
+
+		describe "authorization" do
+
+			describe "for non signed-in-users" do
+				let(:user) { FactoryGirl.create(:user) }
+				
+				describe "when attempting to visit a protected page" do
+					before do
+						visit edit_user_path(user)
+						fill_in "Email", with:user.email
+						fill_in "Password", with:user.password
+						click_button "Sign in"
+					end
+
+					describe "after signing in" do
+						it "should render the desired protected page" do
+							page.should have_title_content('Edit user')
+						end
+					end
+				end
+			end
+
+			let(:user) { FactoryGirl.create(:user) }
+
+			describe "in the Users controller" do
+				before { visit edit_user_path(user) }
+				it { should have_title_content('Sign in') }
+			end
+
+			describe "submitting to the update action" do
+				before { put user_path(user) }
+				specify { response.should redirect_to(signin_path) }
+			end
+
+
+			describe "as wrong user" do
+				let(:user) { FactoryGirl.create(:user) }
+				let(:wrong_user) { FactoryGirl.create(:user, email:"wrong@example.com") }
+				before{ sign_in user }
+
+				describe "visiting Users#edit page" do
+					before { visit edit_user_path(wrong_user) }
+					it { should_not have_title_content('Edit user') }
+				end
+
+				describe "submitting a PUT request to the Users#update action" do
+					before { put user_path(wrong_user) }
+					specify { response.should redirect_to(root_path) }
+				end
+			end
+
+		end
 	end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,4 +1,6 @@
 include ApplicationHelper
+include UsersHelper
+
 def full_title(page_title)
 	base_title = "Ruby on Rails Tutorial Sample App"
 	if page_title.empty?
@@ -16,13 +18,20 @@ RSpec::Matchers.define :have_title_content do |content|
 	end
 end
 
-
+RSpec::Matchers.define :have_h1_content do |content|
+	match do |page|
+		page.should have_selector('h1', text: content)
+	end
+end
 
 #utilities for authentication_pages_spec
-def valid_signin(user)
+def sign_in(user)
+	visit signin_path
 	fill_in "Email",	with: user.email
 	fill_in "Password", with: user.password
 	click_button "Sign in"
+	#Sign in when not using Capybara as well.
+	cookies[:remember_token] = user.remember_token
 end
 
 
@@ -31,5 +40,14 @@ RSpec::Matchers.define :have_error_message do |message|
 		page.should have_selector('div.alert-error', text: message)
 	end
 end
+
+RSpec::Matchers.define :have_success_message do |message|
+	match do |page|
+		page.should have_selector('div.alert-success', text: message)
+	end
+end
+
+#utitilites for user_pages_spec
+
 
 
